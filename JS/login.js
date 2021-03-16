f847f176409a600d07c2e07e41f1e8919570b691
tarique.intern@appscrip.co <paritosh.intern@appscrip.co>
2021-03-16 19:31:39 +0530

    initial commit

---
+++ JS/login.js
@@ -0,0 +1,38 @@
+
+'use strict';
+//declearing html elements
+
+const login_form = document.querySelector("form");
+const userEmail = document.getElementById('userEmail');
+const userPass = document.querySelector('#userPass');
+const login_msg = document.getElementById('login_msg')
+
+
+
+
+// ----login validation
+const userAuthenticate_msg = (user_msg) => {
+
+    document.getElementById('login_msg').innerText = user_msg;
+
+}
+
+login_form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (localStorage.getItem('user')) {
+        const loginDetails = JSON.parse(localStorage.getItem('user'))
+        if ((userEmail.value === loginDetails.email) && (userPass.value === loginDetails.password)) {
+            userAuthenticate_msg("Login-Success...")
+
+        } else {
+            userAuthenticate_msg("Password or Email is wrong !")
+        }
+    } else {
+        userAuthenticate_msg('Not a registered user')
+    }
+    // window.location.href = "login.html";
+
+});
+
+
+
---
+++ JS/register_validater.js
@@ -0,0 +1,85 @@
+'use strict';
+//declearing html elements
+
+const form = document.querySelector("form");
+const userEmail = document.querySelector('#userEmail')
+const userPass = document.getElementById('userPass');
+const userConfirmPass = document.getElementById('userConfirmPass');
+const userFirstName = document.getElementById('userFirstName');
+const userLastName = document.getElementById('userLastName');
+const userBirthDate = document.getElementById('userBirthDate')
+
+// first Name Validator
+userFirstName.addEventListener('blur', () => {
+    let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,20}$/;
+    let firstname = userFirstName.value;
+    return (regex.test(firstname) ? userFirstName.classList.add('is-valid')
+        : userFirstName.classList.add('is-invalid'));
+    
+});
+//  Last Name Validator 
+userLastName.addEventListener('blur', () => {
+    let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,20}$/;
+    let lastname = userLastName.value;
+    return (regex.test(lastname) ? userLastName.classList.add('is-valid')
+        : userLastName.classList.add('is-invalid'));
+
+});
+
+//  Email ID Validator 
+userEmail.addEventListener('blur', () => {
+    let regex = /^[A-Za-z]{1}[A-Za-z._0-9]{3,}@[A_Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/;
+    let emailId = userEmail.value;
+    return (regex.test(emailId) ? userEmail.classList.add('is-valid')
+        : userEmail.classList.add('is-invalid'));
+
+});
+//  Date of Birth Validator 
+userBirthDate.addEventListener('blur', () => {
+    
+    let birthdate =  Date.parse(userBirthDate.value);
+    let currentAge = Date.now();
+    return ((currentAge>=18) ? userBirthDate.classList.add('is-valid')
+        : userBirthDate.classList.add('is-invalid'));
+
+});
+
+//  password Validator 
+userPass.addEventListener('blur', () => {
+    let regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9@!#?]{8,20}$/;
+    let password = userPass.value;
+    return (regex.test(password) ? userPass.classList.add('is-valid')
+        : userPass.classList.add('is-invalid'));
+});
+
+//  Confirm Validator 
+userConfirmPass.addEventListener('blur', () => {
+    console.log("work");
+    let regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9@!#?]{8,20}$/;
+    let confirmPassword = userConfirmPass.value;
+    return (regex.test(confirmPassword) ? userConfirmPass.classList.add('is-valid')
+        : userConfirmPass.classList.add('is-invalid'));
+
+});
+
+// password matching
+
+
+
+// Save the SigneUp Details In Local Storage;
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const userRegisterData = {
+        firstName: userFirstName.value.trim(),
+        lastName: userLastName.value.trim(),
+        birthDate: userBirthDate.value.trim(),
+        email: userEmail.value.trim(),
+        password:  userPass.value.trim(),
+    }
+    localStorage.setItem('user', JSON.stringify(userRegisterData));
+    window.location.href = "login.html";
+    console.log(userRegisterData);
+
+});
