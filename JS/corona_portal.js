8bae909bf4c9e157d36dc2f11d73bc60314859b6
tarique.intern@appscrip.co <paritosh.intern@appscrip.co>
2021-03-16 22:37:55 +0530

    covidpanel updated

--- JS/corona_portal.js
+++ JS/corona_portal.js
@@ -1,10 +1,6 @@
 'use strict';
 //declearing html elements
 
-const userEmail = document.getElementById('selected_state_count');
-const userPass = document.querySelector('Tested_case_count');
-const confirm_case = document.getElementById('confirm_case_count');
-const recover_case = document.getElementById('recover_case_count');
 const statecode = document.querySelector('.all_state_section');
 
 let state_arr = [];
@@ -14,51 +10,55 @@ function state_case_count(count){
     
     document.getElementById('selected_state_count').innerText = count+1;
 };
-// total recovered confirm, tested cases
-function allcases(covid19india){
-    let totalConfirmed = 0;
-    let totalRecovered = 0;
-    let totalActive = 0;
-    for(let cases in covid19india){
 
-        console.log(covid19india[caches]);
-    }
-    
-    // console.log(covid19india);
+function renderStatesCode(codesArray) {
+    codesArray.map(eachCode => {
+        statecode.insertAdjacentHTML('beforeend',
+                `<div class="from-group form-check py-1" id="${eachCode.statecode}">
+                <input class="form-check-input" type="checkbox" value="" checked>
+            <label class="form-check-label"> ${eachCode.statecode} </label>
+        </div>`);
+    })
 }
+
+function renderCalsReports (selectState,tested,confirmed,recovered){
+    document.getElementById('selected_state_count').innerText = selectState;
+    document.querySelector('#Tested_case_count').innerText = tested;
+    document.querySelector('#confirm_case_count').innerText = confirmed;
+    document.querySelector('#recover_case_count').innerText = recovered;
+
+}
+
+function renderReports(disrtictData) {
+    let selectState =  0
+    let tested = 0
+    let confirmed = 0 
+    let recovered = 0
+    // console.log(disrtictData)
+    disrtictData.map(disrtictData => {
+        selectState+= 1
+        Object.values(disrtictData.districtData).map(data => {
+            confirmed +=  data.confirmed
+            recovered += data.recovered
+            tested += data.active + data.confirmed + data.recovered
+
+        })
+    })
+    renderCalsReports (selectState,tested,confirmed,recovered);
+    // console.log(selectState,tested,confirmed,recovered)
+
+}
+
 
 //fetching covid data district wise
 const fetchCovidData = () => {
     fetch('https://api.covid19india.org/state_district_wise.json')
         .then(response => response.json())
         .then(data => {
-
-            var state;
-            var countState = 0;
-            for (state in data) {
-                state_arr.push(data[state]['statecode']);
-
-                allcases(data[state]['districtData']);
-
-                state_case_count(countState++);
-            };
-           
-            const state_covid_arr = (state_arr1) => {
-                state_arr1.forEach(state_covid19 => {
-
-                    statecode.insertAdjacentHTML('beforeend',
-                        `<div class="from-group form-check py-1" id="${state_covid19}">
-                        <input class="form-check-input" type="checkbox" value="" checked>
-                    <label class="form-check-label"> ${state_covid19} </label>
-                </div>`
-
-                    );
-
-                });
-            }
-
-            state_covid_arr(state_arr);
-
+            const disrtictData = Object.values(data)
+            renderStatesCode(disrtictData)
+            renderReports(disrtictData)
+            
         })
         .catch(error => console.error(error))
 
