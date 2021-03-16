d429860642e8506d39f4cbc9e7d5f2c4a913f943
tarique.intern@appscrip.co <paritosh.intern@appscrip.co>
2021-03-16 19:55:27 +0530

    files updated

--- JS/login.js
+++ JS/login.js
@@ -14,6 +14,7 @@ const login_msg = document.getElementById('login_msg')
 const userAuthenticate_msg = (user_msg) => {
 
     document.getElementById('login_msg').innerText = user_msg;
+    
 
 }
 
@@ -23,6 +24,8 @@ login_form.addEventListener('submit', (e) => {
         const loginDetails = JSON.parse(localStorage.getItem('user'))
         if ((userEmail.value === loginDetails.email) && (userPass.value === loginDetails.password)) {
             userAuthenticate_msg("Login-Success...")
+                window.location.href = "corona_portal.html";
+
 
         } else {
             userAuthenticate_msg("Password or Email is wrong !")
@@ -30,7 +33,6 @@ login_form.addEventListener('submit', (e) => {
     } else {
         userAuthenticate_msg('Not a registered user')
     }
-    // window.location.href = "login.html";
 
 });
 
